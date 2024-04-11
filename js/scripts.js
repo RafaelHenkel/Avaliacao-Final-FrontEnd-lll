6bc0f608a4a11eda02b195cbeec6fb02a055df97
RafaelH <rafael0henkel1@gmail.com>
2024-04-11 14:16:16 -0300

    create cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-const api = axios.create({
-  baseURL: "https://rickandmortyapi.com/api",
-});
-
 const divFooter = document.getElementById("info-footer");
+const rowCards = document.getElementById("row-cards");
 
-const getCharacter = async () => {
+let acc = 1;
+
+const getInfos = async () => {
   try {
     const character = await api.get("/character");
     const amountCharacter = character.data.info.count;
@@ -21,4 +20,70 @@ const getCharacter = async () => {
   }
 };
 
-getCharacter();
+getInfos();
+
+const getCharacters = async () => {
+  try {
+    const response = await api.get(`character/[${acc++},${acc++},${acc++},${acc++},${acc++},${acc++}]`);
+    const characters = response.data;
+
+
+
+
+    characters.forEach((character, index) => {
+      let htmlCard = `
+      <div class="col-6">
+        <div class="card mb-3">
+          <div class="row g-0" id="row-cards">
+            <div class="col-md-5">
+              <img src="${character.image}" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <h2 class="card-title text-white fw-bold">${character.name}</h2>
+                <p class="card-text text-white status-text">
+                <span class="dead"></span>
+                ${character.status} - ${character.species}
+                </p>
+                <p class="card-text my-text-body">
+                  Última localização conhecida </br>
+                  <span class="text-white">${character.location.name}</span>
+                </p>
+                <p class="card-text ">
+                </p>
+                <p class="card-text my-text-body">
+                  Último episódio visto: </br>
+                  <span class="text-white">${character.location.name}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+
+      if (index > 4) {
+        htmlCard = '<div class="col-6"></div>' + htmlCard;
+      }
+
+      rowCards.innerHTML += htmlCard;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getCharacters();
+
+const prevPage = () => {
+  if (acc > 7) {
+    acc -= 12;
+    rowCards.innerHTML = ``;
+    getCharacters();
+  }
+};
+
+const nextPage = () => {
+  rowCards.innerHTML = ``;
+  getCharacters();
+};
