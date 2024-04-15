0f62584cbaa41f5af8f467759506dbbfac44d24d
RafaelH <rafael0henkel1@gmail.com>
2024-04-15 16:38:52 -0300

    js formating

--- js/scripts.js
+++ js/scripts.js
@@ -15,63 +15,63 @@ api
         .get(episodeUrl)
         .then(function (episode) {
           let htmlCard = `
-<div class="col-6">
-  <div class="card mb-3">
-    <div class="row g-0" id="row-cards">
-      <div class="col-md-5">
-        <img src="${character.image}" class="img-fluid rounded-start" alt="..." />
-      </div>
-      <div class="col-md-7">
-        <div class="card-body">
-        <a href='' data-bs-toggle="modal" data-bs-target="#exampleModal${character.id}"class="card-title text-white">
-          ${character.name}
-        </a>
-          <p class="card-text text-white status-text">
-            span class="${character.status}"></span>
-          ${character.status} - ${character.species}
-          </p>
-          <p class="card-text my-text-body">
-            Última localização conhecida </br>
-            <span class="text-white">${character.location.name}</span>
-          </p>
-          <p class="card-text ">
-          </p>
-          <p class="card-text my-text-body">
-            Último episódio visto: </br>
-            <span class="text-white">${episode.data.name}</span>
-          </p>
+      <div class="col-6">
+        <div class="card mb-3">
+          <div class="row g-0" id="row-cards">
+            <div class="col-md-5">
+              <img src="${character.image}" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+              <a href='' data-bs-toggle="modal" data-bs-target="#exampleModal${character.id}"class="card-title text-white">
+                ${character.name}
+              </a>
+                <p class="card-text text-white status-text">
+                  span class="${character.status}"></span>
+                ${character.status} - ${character.species}
+                </p>
+                <p class="card-text my-text-body">
+                  Última localização conhecida </br>
+                  <span class="text-white">${character.location.name}</span>
+                </p>
+                <p class="card-text ">
+                </p>
+                <p class="card-text my-text-body">
+                  Último episódio visto: </br>
+                  <span class="text-white">${episode.data.name}</span>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-<div class="modal fade" id="exampleModal${character.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5 fw-bold" id="exampleModalLabel">${character.name}</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body d-flex flex-column">
-        <img src="${character.image}" alt="" />
-        <div class="mt-3">
-        <p class="card-text text-white status-text">
-         Status:   
-        ${character.status}
-          <span class="${character.status} mx-2"></span>
-        </p>
-          <p>Especie: ${character.species}</p>
-          <p>Gênero: ${character.gender}</p>
-          <p>Origem: ${character.origin.name}</p>
+      <div class="modal fade" id="exampleModal${character.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5 fw-bold" id="exampleModalLabel">${character.name}</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex flex-column">
+              <img src="${character.image}" alt="" />
+              <div class="mt-3">
+              <p class="card-text text-white status-text">
+              Status:   
+              ${character.status}
+                <span class="${character.status} mx-2"></span>
+              </p>
+                <p>Especie: ${character.species}</p>
+                <p>Gênero: ${character.gender}</p>
+                <p>Origem: ${character.origin.name}</p>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Fechar</button>
-      </div>
-    </div>
-  </div>
-</div>
-`;
+      `;
 
           if (index > 4) {
             htmlCard = '<div class="col-6"></div>' + htmlCard;
@@ -194,7 +194,7 @@ const searchCharacter = async () => {
           </div>
         </div>
       </div>
-  `;
+      `;
         if (index > 4) {
           htmlCard = '<div class="col-6"></div>' + htmlCard;
         }
